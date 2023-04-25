2ebcdea63086d965a0a83d4ee5824ba9ffd917f1
Donald Miller <admin@evalgenie.com>
2023-04-24 21:55:05 -0400

    initial development

---
+++ chatgpt.py
@@ -0,0 +1,21 @@
+from dotenv import dotenv_values
+import openai
+
+config = dotenv_values('.env')
+# print(config['OPENAI_AIP_KEY'])
+
+openai.api_key = config['OPENAI_AIP_KEY']
+messages = [ {"role": "system", "content": 
+              "You are a intelligent assistant."} ]
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
